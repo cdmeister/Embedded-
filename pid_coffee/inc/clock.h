00a4fd1cdbcf155f4121559dc61cf8e878b2c75a
Moeiz Riaz <moeizr@umich.edu>
2018-05-26 19:13:59 -0700

    adding more clock code.

---
+++ pid_coffee/inc/clock.h
@@ -0,0 +1,31 @@
+/**
+  ******************************************************************************
+  * @file    lcd.h
+  * @author  Moeiz Riaz
+  * @version V1.0.0
+  * @date    30-April-2018
+  * @brief   Header for clocks.
+  ******************************************************************************
+*/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __CLOCK_H
+#define __CLOCK_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx.h"
+#include "system_stm32f4xx.h"
+#include "systick.h"
+#include "lcd.h"
+#include "string.h"
+#include "stdlib.h"
+#include "stdio.h"
+#include "stdarg.h"
+/* Exported types ------------------------------------------------------------*/
+
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+
+/* Exported functions ------------------------------------------------------- */
+
+#endif /* __CLOCK_H */
---
+++ pid_coffee/src/clock.c
@@ -0,0 +1,175 @@
+#include "clock.h"
+
+static uint8_t APBxPrescTable[4]={2,4,8,16};
+
+void SystemHSIenable(uint16_t ahb_prescaler, uint8_t  apb1_prescaler,
+                      uint8_t apb2_prescaler){
+
+  /* Read the SWS to know what is the current clock selected */
+  uint32_t current_clock = RCC->CFGR & RCC_CFGR_SWS;
+
+  /* Enable HSI clock */
+	RCC->CR |= RCC_CR_HSION;
+
+	/* Wait till HSI is ready */
+	while (!(RCC->CR & RCC_CR_HSIRDY)){};
+
+  /* Follow Procedure for increasing cpu frequency, page 81 ref manual*/
+  if(current_clock == RCC_CFGR_SWS_HSE){
+
+    /* Program the wait states */
+    FLASH->ACR = (FLASH->ACR & ~(FLASH_ACR_LATENCY)) |FLASH_ACR_LATENCY_0WS;
+
+    /* Select HSI clock as main clock */
+	  RCC->CFGR = (RCC->CFGR & ~(RCC_CFGR_SW)) | RCC_CFGR_SW_HSI;
+    /* wait til HSI is selected as main clock */
+    while((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSI){}
+
+    /* Set the prescalers accordingly for HSI */
+    SystemClockPrescaler(ahb_prescaler,apb1_prescaler,apb2_prescaler);
+
+    RCC->CR &=~(RCC_CR_HSEON);
+
+  }
+  /* Follow Procedure for decreasing cpu frequency, page 81 ref manual */
+  else{
+
+    /* Select HSI clock as main clock */
+	  RCC->CFGR = (RCC->CFGR & ~(RCC_CFGR_SW)) | RCC_CFGR_SW_HSI;
+    /* wait til HSI is selected as main clock */
+    while((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSI){}
+
+    /* Set the prescalers accordingly for HSI */
+    SystemClockPrescaler(ahb_prescaler,apb1_prescaler,apb2_prescaler);
+
+    /* Program the wait states */
+    FLASH->ACR = (FLASH->ACR & ~(FLASH_ACR_LATENCY)) |FLASH_ACR_LATENCY_0WS;
+
+    RCC->CR &=~(RCC_CR_PLLON);
+  }
+  SystemCoreClockUpdate();
+}
+
+void SystemClockPrescaler(uint16_t ahb_prescaler, uint8_t  apb1_prescaler,
+                      uint8_t apb2_prescaler){
+
+    /* AHB Clock Divider */
+    RCC->CFGR &= ~(RCC_CFGR_HPRE);
+
+    /* APB1 Clock Divider */
+    RCC->CFGR &= ~(RCC_CFGR_PPRE1);
+
+    /* APB2 Clock Divider */
+    RCC->CFGR &= ~(RCC_CFGR_PPRE2);
+
+
+  /* HPRE( aka AHB) prescaler setting*/
+  switch ( ahb_prescaler){
+    case 2:
+      RCC->CFGR |= RCC_CFGR_HPRE_DIV2;
+      break;
+    case 4:
+      RCC->CFGR |= RCC_CFGR_HPRE_DIV4;
+      break;
+    case 8:
+      RCC->CFGR |= RCC_CFGR_HPRE_DIV8;
+      break;
+    case 16:
+      RCC->CFGR |= RCC_CFGR_HPRE_DIV16;
+      break;
+    case 64:
+      RCC->CFGR |= RCC_CFGR_HPRE_DIV64;
+      break;
+    case 128:
+      RCC->CFGR |= RCC_CFGR_HPRE_DIV128;
+      break;
+    case 256:
+      RCC->CFGR |= RCC_CFGR_HPRE_DIV256;
+      break;
+    case 512:
+      RCC->CFGR |= RCC_CFGR_HPRE_DIV512;
+      break;
+    default:
+      RCC->CFGR |= RCC_CFGR_HPRE_DIV1;
+      break;
+  }
+  /* PPRE1(APB1) prescaler setting*/
+  switch (apb1_prescaler){
+    case 2:
+      RCC->CFGR |= RCC_CFGR_PPRE1_DIV2;
+      break;
+    case 4:
+      RCC->CFGR |= RCC_CFGR_PPRE1_DIV4;
+      break;
+    case 8:
+      RCC->CFGR |= RCC_CFGR_PPRE1_DIV8;
+      break;
+    case 16:
+      RCC->CFGR |= RCC_CFGR_PPRE1_DIV16;
+      break;
+    default:
+      RCC->CFGR |= RCC_CFGR_PPRE1_DIV1;
+      break;
+
+  }
+
+  /* PPRE2(APB2) prescaler setting*/
+  switch (apb2_prescaler){
+
+    case 2:
+      RCC->CFGR |= RCC_CFGR_PPRE2_DIV2;
+      break;
+    case 4:
+      RCC->CFGR |= RCC_CFGR_PPRE2_DIV4;
+      break;
+    case 8:
+      RCC->CFGR |= RCC_CFGR_PPRE2_DIV8;
+      break;
+    case 16:
+      RCC->CFGR |= RCC_CFGR_PPRE2_DIV16;
+      break;
+    default:
+      RCC->CFGR |= RCC_CFGR_PPRE2_DIV1;
+      break;
+
+  }
+
+}
+
+uint16_t GetAHBPrescaler(void){
+
+  uint16_t ahb_prescaler = ((RCC->CFGR & RCC_CFGR_HPRE) >> RCC_CFGR_HPRE_Pos);
+  if (ahb_prescaler < 0x8) return 1;
+  else return ahb_prescaler;
+
+}
+
+uint8_t GetAPB1Prescaler(void){
+
+  uint8_t apb1_prescaler = ((RCC->CFGR & RCC_CFGR_PPRE1) >> RCC_CFGR_PPRE1_Pos);
+  if (apb1_prescaler < 0x4) return 1;
+  else return APBxPrescTable[apb1_prescaler&0x3];
+
+}
+
+uint8_t GetAPB2Prescaler(void){
+
+  uint8_t apb2_prescaler = ((RCC->CFGR & RCC_CFGR_PPRE2) >> RCC_CFGR_PPRE2_Pos);
+  if (apb2_prescaler < 0x4) return 1;
+  else return APBxPrescTable[apb2_prescaler&0x3];
+
+}
+
+uint32_t GetHCLK(void){
+
+  uint16_t hpre_prescaler = GetAHBPrescaler();
+  return SystemCoreClock/hpre_prescaler;
+
+}
+
+uint32_t GetPCLK2(void){
+  uint32_t HCLK = GetHCLK();
+  uint8_t apb2_prescaler = GetAPB2Prescaler();
+  return HCLK/apb2_prescaler;
+
+}
